1a99c22d4d2aba56bf20ca0a86b5a591fb013775
vsevolodsh <86848476+vsevolodsh@users.noreply.github.com>
2023-04-10 20:40:51 +0400

    Delete ViewPrPrGroups.sql

--- ViewPrPrGroups.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW PrPrGroups AS
-SELECT Product.Number, Product.Name as Name, ProductGroup.Name AS ProductGroup, Product.RentCostInHour,  Product.IsProductInWarehouse
-FROM Product
-JOIN ProductGroup ON ProductGroup.Code = Product.Code AND ProductGroup.ProductSubcategoryCode = Product.ProductSubcategoryCode
