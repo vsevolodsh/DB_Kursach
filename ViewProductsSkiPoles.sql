9f20814db06bc13d7f9b8f77f8b633fc934069eb
vsevolodsh <86848476+vsevolodsh@users.noreply.github.com>
2023-04-10 20:40:44 +0400

    Delete ViewProductsSkiPoles.sql

--- ViewProductsSkiPoles.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW ProductsSkiPoles AS
-SELECT PrPrGroups.Number, PrPrGroups.ProductGroup, PrPrGroups.Name, SkiPoles.HandleMaterial, SkiPoles.ShaftMaterial, SkiPoles.SkiPolesLength, PrPrGroups.RentCostInHour, PrPrGroups.IsProductInWarehouse
-FROM PrPrGroups 
-JOIN SkiPoles ON SkiPoles.ProductNumber = PrPrGroups.Number 
